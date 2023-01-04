7c9a92c4818749436e6bf6457e19cc43747bf0d9
Hanna <hanna.kim.x@gmail.com>
2023-01-04 12:17:06 -0800

    add styles to flip card

--- index/index.js
+++ index/index.js
@@ -1,3 +1,14 @@
 // console.log("js file is linked")
 
-// 
+const game = document.querySelectorAll('.board')
+
+// flip card when clicked
+const cards = document.querySelectorAll('.card-item')
+
+function flipCard() {
+    this.classList.toggle('flip')
+
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard))
+
